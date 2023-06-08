f32d1e27c7cb71197e69bc1c0b757a9f3aae663f
zhang <2394668099@qq.com>
2023-06-08 20:23:42 +0800

    feat: 添加 prettierrc 及 editorconfig 配置

--- shared/write.js
+++ shared/write.js
@@ -1,11 +1,19 @@
 const fs = require("fs");
 const { SHAPE_FLAGS } = require("./shapeFlags");
-const { LICENSE, GITIGNORE, ESLINTVUE } = require("../constants/templateName");
+const {
+  LICENSE,
+  GITIGNORE,
+  ESLINTVUE,
+  PRETTIER,
+  EDITORCONFIG,
+} = require("../constants/templateName");
 function writeFileSync(context, data) {
   let file;
   if (
     context.shapeFlag === SHAPE_FLAGS[LICENSE] ||
-    context.shapeFlag === SHAPE_FLAGS[GITIGNORE]
+    context.shapeFlag === SHAPE_FLAGS[GITIGNORE] ||
+    context.shapeFlag === SHAPE_FLAGS[PRETTIER] ||
+    context.shapeFlag === SHAPE_FLAGS[EDITORCONFIG]
   ) {
     file = `${context.mkPath}/${context.file}`;
   } else {
